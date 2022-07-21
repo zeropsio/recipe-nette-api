5f8d4dbe38ffcf9e45ebcebb0e7f59851232953c
Narek Vardanjan <vardanar@fit.cvut.cz>
2022-07-21 10:00:17 +0200

    Fix CORS

--- www/index.php
+++ www/index.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
+header('Access-Control-Allow-Headers: accept, content-type, authorization');
+header('Access-Control-Allow-Methods: GET,POST,OPTIONS,DELETE,PUT,PATCH');
+header('Access-Control-Allow-Origin: *');
+if ("OPTIONS" === $_SERVER['REQUEST_METHOD']) {
+    return;
+}
+
 $configurator = App\Bootstrap::boot();
 $container = $configurator->createContainer();
 $application = $container->getByType(Nette\Application\Application::class);
