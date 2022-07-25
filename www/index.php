c1c04b3a5aa2f95aef3950ac543a14d221034a6f
Narek Vardanjan <vardanar@fit.cvut.cz>
2022-07-25 16:27:40 +0200

    Fix CORS via PSR-7 middleware

--- www/index.php
+++ www/index.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-header('Access-Control-Allow-Headers: accept, content-type, authorization');
+/*header('Access-Control-Allow-Headers: accept, content-type, authorization');
 header('Access-Control-Allow-Methods: GET,POST,OPTIONS,DELETE,PUT,PATCH');
 header('Access-Control-Allow-Origin: *');
 if ("OPTIONS" === $_SERVER['REQUEST_METHOD']) {
     return;
-}
+}*/
 
 $configurator = App\Bootstrap::boot();
 $container = $configurator->createContainer();
-$application = $container->getByType(Nette\Application\Application::class);
+$application = $container->getByType(Contributte\Middlewares\Application\IApplication::class);
 $application->run();
